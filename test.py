2619807d188a6210061b4e6fe0f58216b3833260
Your Name <your.email@example.com>
2024-12-24 15:41:10 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,23 @@
+# //git github
+
+
+# 1 git init (гиттти инитсилизация кылуу)
+
+
+
+
+
+# 2 git add . либо по называнию файла
+
+
+
+# 3 git commit -m " men regustrtion buttum"
+
+
+
+#  4  git branc main
+
+#  5 git remote add origin https://github.com/Jeenbekov000123/bootcamp.05.git
+
+
+# 6 git push -u origin  moin
